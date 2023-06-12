3bb6a5d19bbcf2df3e972025f4dee9578e84bac6
nguyenvipklkt <nguyenhua2508@gmail.com>
2023-06-12 17:00:52 +0700

    buyTicket

--- src/views/buyTicket.js
+++ src/views/buyTicket.js
@@ -4,7 +4,11 @@ import '../assets/css/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown, faLocationDot, faCalendar, faClock, faPersonCircleCheck, faTrophy, faCalendarDays, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import firstLogo from '../assets/imgs/firstLogo/logo.png';
-import triky from '../assets/imgs/filmbanner/triky.jpg'
+import axios from 'axios';
+import moment from 'moment';
+import lastLogo from '../assets/imgs/Last-Logo/favicon-large.png';
+import beta from '../assets/imgs/Partner/beta-cineplex-v2.jpg';
+import cinemax from '../assets/imgs/Partner/cinemax.png';
 
 const BuyTicket = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -58,6 +62,83 @@ const BuyTicket = () => {
         }
     }
 
+    // xử lý cinema
+
+    const [location, setLocation] = useState('Hà Nội');
+    const [format, setFormat] = useState('Định dạng');
+    const [choose, setChoose] = useState(false);
+    const [isShow, setIsShow] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [clicked, setClicked] = useState(null);
+    const showLocation = () => {
+        setIsShow(!isShow)
+    }
+    const locations = [
+        'Tp. Hồ Chí Minh',
+        'Hà Nội',
+    ]
+    const formats = ['Định dạng', '2D', '3D']
+
+    // lấy dữ liệu dataCinema từ backend
+    const [dataCinema, setDataCinema] = useState([]);
+
+    const times = ['19:00', '20:00', '20:40', '21:20', '22:20', '23:00', '23:40', '23:55']
+
+    useEffect(() => {
+        axios.get("http://localhost:4000/api/v1/cinemas")
+            .then(response => {
+                const jsonData = JSON.parse(JSON.stringify(response.data.dataCinema));
+                setDataCinema(jsonData); // Dữ liệu đã được chuyển đổi thành đối tượng JSON
+                console.log(jsonData);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+    const getCinemaById = (id) => {
+        return dataCinema.find(cinema => cinema.idCinema === id);
+    };
+
+    // tạo mảng idCinemas chứa idCinema
+    const idCinemas = dataCinema.map(cinema => cinema.idCinema);
+    const CinemasCard = ({ id }) => {
+        const cinema = getCinemaById(id);
+        return (
+            <div>
+                <li>
+                    <div className='info-container'>
+                        <div className='cine-logo'>
+                            <img src={cinema.logo} class='cinema-logo' alt=''></img>
+                            <div className="cine-info">
+                                <p style={{ fontWeight: 600 }}>{cinema.showRoom}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <ul className='ul-child'>
+                            {times.map((time) => (
+                                <li>{time}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </li>
+            </div>
+        )
+    }
+
+
+    // Lấy ngày hôm nay
+    const today = moment().format('DD/MM');
+
+    // Lấy 4 ngày tiếp theo
+    const nextDays = [];
+    for (let i = 1; i <= 6; i++) {
+        const nextDay = moment().add(i, 'days').format('DD/MM');
+        nextDays.push(nextDay);
+    }
+
+
     return (
         <div>
             {/* header  */}
@@ -95,6 +176,9 @@ const BuyTicket = () => {
             {/* header  */}
 
             {/* body  */}
+
+            {/* inf-film */}
+
             <div id="inf-film-content">
                 <div class="inf-film">
                     <img src={film.Image} alt="" />
@@ -112,7 +196,7 @@ const BuyTicket = () => {
                                 <div class="title-premiere"><FontAwesomeIcon icon={faCalendar} /> Khởi chiếu</div>
                                 <div class="content-premiere content-ex">{getFormattedDate(film.releaseDate)}</div>
                             </div>
-                            <div class="time ex">
+                            <div class="-time ex">
                                 <div class="title-time"><FontAwesomeIcon icon={faClock} /> Thời lượng</div>
                                 <div class="content-time content-ex">{film.time} phút</div>
                             </div>
@@ -131,7 +215,106 @@ const BuyTicket = () => {
                     </div>
                 </div>
             </div>
+            {/* inf-film */}
+
+            {/* cinema */}
+
+            {/* options */}
+            <div class="options">
+                <div class="location" onClick={showLocation}>
+                    {location}
+                    {
+                        isShow && (
+                            <div className='location-list'>
+                                <ul>
+                                    {locations.map((location) => (
+                                        <li key={location} onClick={() => setLocation(location)}>{location}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className='format' onClick={() => setChoose(!choose)}>
+                    <p>{format}</p>
+                    <FontAwesomeIcon icon={faCaretDown} />
+                    {
+                        choose && (
+                            <div className='format-choose'>
+                                <ul>
+                                    {formats.map((format) => (
+                                        <li key={format} onClick={() => setFormat(format)}>{format}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+            {/* options */}
+
+            {/* thời gian  */}
+            <div class="time">
+                <ul>
+                    {nextDays.map((day, index) => (
+                        <li onClick={() => setSelectedItem(index)} style={{ backgroundColor: selectedItem === index ? '#C7D6EC' : '#EDF2F9' }}>{day}</li>
+                        // <li key={index}>{day}</li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* thời gian  */}
+
+            {/* inf-cinema */}
+            <div class="cinema">
+                <ul className='cinema-ul'>
+                    <div>
+                        {idCinemas.map(idCinema => (
+                            <CinemasCard key={idCinema} id={idCinema} />
+                        ))}
+                    </div>
+                </ul>
+            </div>
+            {/* inf-cinema */}
+
+            {/* cinema */}
+
             {/* body  */}
+
+            <div class="margin-40px"> </div>
+
+            {/* footer */}
+            <div id="footer">
+                <div class="wraper">
+                    <div class="logo">
+                        <img src={lastLogo} alt="img" class="img-logo" />
+                    </div>
+                    <div class="inf">
+                        <div class="text-inf-heading">CÔNG TY TNHH MONET</div>
+                        <p class="text-inf">Số ĐKKD: 0315367026 · <br />Nơi cấp: Đại học Bách Khoa Hà Nội Tp. Hà Nội · <br />Đăng ký lần đầu
+                            ngày 01/11/2018
+                            <br />Địa chỉ: · v8.1
+                        </p>
+                    </div>
+                    <div class="partner">
+                        <div class="a-partner">Đối tác : </div>
+                        <ul class="b-partner">
+                            <li><img src={beta} alt="img" /></li>
+                            <li><img src={cinemax} alt="img" /></li>
+                        </ul>
+                    </div>
+                    <div class="policy">
+                        <ul class="-policy">
+                            <li><a href="">Về chúng tôi</a></li>
+                            <li><a href="">Chính sách và thỏa thuận</a></li>
+                            <li><a href="">Hỗ trợ</a></li>
+                            <li><a href="">liên hệ</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            {/* footer */}
+
         </div>
     )
 }
